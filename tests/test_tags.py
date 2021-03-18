c527b4ab029b428da3b83292ccd7e0ee75a87cfa
UshakovAleksandr <sashapimp@mail.ru>
2021-03-18 12:54:30 +0300

    Change tag and user tests

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -3,9 +3,11 @@ from api import db
 from app import app, Config
 from unittest import TestCase
 from api.models.tag import TagModel
+from api.models.user import UserModel
+from base64 import b64encode
 
 
-class TestUsers(TestCase):
+class TestTags(TestCase):
     def setUp(self):
         self.app = app
         self.app.config.update({
@@ -15,27 +17,39 @@ class TestUsers(TestCase):
         with self.app.app_context():
             db.create_all()
 
-    def test_get_tag_by_id(self):
+    def create_test_user1(self):
+        user_data = {
+            "username": 'admin',
+            'password': 'admin'
+        }
+
+        user = UserModel(**user_data)
+        user.save()
+        res = self.client.get("/users")
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data[0]["username"], user_data["username"])
+        return user
+
+    def create_test_tag1_by_user1(self):
+        user = TestTags.create_test_user1(self)
         tag_data = {
             "name": 'test tag'
         }
 
-        tag = TagModel(**tag_data)
-        tag.save()
-        tag_id = tag.id
+        headers = {
+            'Authorization': 'Basic ' + b64encode(
+                f"{user.username}:{'admin'}".encode('ascii')).decode('utf-8')
+        }
 
-        res = self.client.get(f"/tags/{tag_id}")
-        self.assertEqual(res.status_code, 200)
+        res = self.client.post("/tags", headers=headers,  data=json.dumps(tag_data), content_type="application/json")
+        self.assertEqual(res.status_code, 201)
         data = json.loads(res.data)
         self.assertEqual(data["name"], tag_data["name"])
+        return user
 
-    def test_get_tag_by_id_not_found(self):
-        res = self.client.get(f"/tags/1")
-        self.assertEqual(res.status_code, 404)
-        data = json.loads(res.data)
-        self.assertEqual(data["error"], "Tag with id=1 not found")
-
-    def test_get_tags(self):
+    def create_test_tag1_and_tag2_by_user1(self):
+        user = TestTags.create_test_user1(self)
         tags_data = [
             {
                 "name": 'test1 tag'
@@ -45,45 +59,68 @@ class TestUsers(TestCase):
             }
         ]
 
-        for tag_data in tags_data:
-            tag = TagModel(**tag_data)
-            tag.save()
+        headers = {
+            'Authorization': 'Basic ' + b64encode(
+                f"{user.username}:{'admin'}".encode('ascii')).decode('utf-8')
+        }
 
-        res = self.client.get("/tags")
+        for tag in tags_data:
+            res = self.client.post("/tags", headers=headers, data=json.dumps(tag), content_type="application/json")
+            self.assertEqual(res.status_code, 201)
+            data = json.loads(res.data)
+            self.assertEqual(data["name"], tag["name"])
+        return user
+
+    def auth_headers(self, user):
+        return {
+            'Authorization': 'Basic ' + b64encode(
+                f"{user.username}:{'admin'}".encode('ascii')).decode('utf-8')
+        }
+
+    def test_get_tag_by_id(self):
+        user = TestTags.create_test_tag1_by_user1(self)
+
+        res = self.client.get("/tags/1", headers=TestTags.auth_headers(self, user))
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.data)
-        self.assertEqual(data[0]["name"], tags_data[0]["name"])
-        self.assertEqual(data[1]["name"], tags_data[1]["name"])
+        self.assertEqual(data["name"], "test tag")
+
+    def test_get_tag_by_id_not_found(self):
+        user = TestTags.create_test_user1(self)
+
+        res = self.client.get(f"/tags/1", headers=TestTags.auth_headers(self, user))
+        self.assertEqual(res.status_code, 404)
+        data = json.loads(res.data)
+        self.assertEqual(data["error"], "Tag with id=1 not found")
+
+    def test_get_tags(self):
+        user = TestTags.create_test_tag1_and_tag2_by_user1(self)
+
+        res = self.client.get("/tags", headers=TestTags.auth_headers(self, user))
+        self.assertEqual(res.status_code, 200)
+        data = json.loads(res.data)
+        self.assertEqual(data[0]["name"], "test1 tag")
+        self.assertEqual(data[1]["name"], "test2 tag")
 
     def test_get_tags_not_found(self):
-        res = self.client.get("/tags")
+        user = TestTags.create_test_user1(self)
+
+        res = self.client.get("/tags", headers=TestTags.auth_headers(self, user))
         self.assertEqual(res.status_code, 404)
         data = json.loads(res.data)
         self.assertEqual(data["error"], "No tags yet")
 
     def test_post_tag_creation(self):
-        tag_data = {
-            "name": 'test tag'
-        }
-
-        res = self.client.post("/tags", data=json.dumps(tag_data), content_type="application/json")
-        self.assertEqual(res.status_code, 201)
-        data = json.loads(res.data)
-        self.assertEqual(data["name"], tag_data["name"])
+        TestTags.create_test_tag1_by_user1(self)
 
     def test_post_tag_creation_the_same_name(self):
-        tag_data = {
-            "name": 'test tag'
-        }
-        res = self.client.post("/tags", data=json.dumps(tag_data), content_type="application/json")
-        self.assertEqual(res.status_code, 201)
-        data = json.loads(res.data)
-        self.assertEqual(data["name"], tag_data["name"])
+        user = TestTags.create_test_tag1_by_user1(self)
 
         tag_data = {
             "name": 'test tag'
         }
-        res = self.client.post("/tags", data=json.dumps(tag_data), content_type="application/json")
+        res = self.client.post("/tags", headers=TestTags.auth_headers(self, user),
+                               data=json.dumps(tag_data), content_type="application/json")
         self.assertEqual(res.status_code, 404)
         data = json.loads(res.data)
         self.assertEqual(data["error"], "An error occurred while adding new tag" 
@@ -91,99 +128,57 @@ class TestUsers(TestCase):
                                         "You can only add a unique tag")
 
     def test_put_tag_by_id(self):
-        tag_data = {
-            "name": 'test tag'
-        }
-
-        tag = TagModel(**tag_data)
-        tag.save()
-        tag_id = tag.id
-
-        res = self.client.get(f"/tags/{tag_id}")
-        self.assertEqual(res.status_code, 200)
-        data = json.loads(res.data)
-        self.assertEqual(data["name"], tag_data["name"])
+        user = TestTags.create_test_tag1_by_user1(self)
 
         tag_data_to_change = {
-            "name": 'test tag1'
+            "name": 'test3 tag'
         }
 
-        res = self.client.put(f"tags/{tag_id}", data=json.dumps(tag_data_to_change),
-                              content_type="application/json")
+        res = self.client.put("tags/1", headers=TestTags.auth_headers(self, user),
+                              data=json.dumps(tag_data_to_change), content_type="application/json")
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.data)
         self.assertEqual(data["name"], tag_data_to_change["name"])
 
     def test_put_tag_by_id_not_found(self):
+        user = TestTags.create_test_user1(self)
+
         tag_data_to_change = {
-            "name": 'test tag1'
+            "name": 'test3 tag'
         }
 
-        res = self.client.put("tags/1", data=json.dumps(tag_data_to_change),
-                              content_type="application/json")
+        res = self.client.put("tags/1", headers=TestTags.auth_headers(self, user),
+                              data=json.dumps(tag_data_to_change), content_type="application/json")
         self.assertEqual(res.status_code, 404)
         data = json.loads(res.data)
         self.assertEqual(data["error"], "Tag with id=1 not found")
 
     def test_put_tag_by_id_change_to_the_same_name(self):
-        tag_data1 = {
-            "name": 'test tag 1'
-        }
-
-        tag1 = TagModel(**tag_data1)
-        tag1.save()
-        tag1_id = tag1.id
-
-        res = self.client.get(f"/tags/{tag1_id}")
-        self.assertEqual(res.status_code, 200)
-        data = json.loads(res.data)
-        self.assertEqual(data["name"], tag_data1["name"])
-
-        tag_data2 = {
-            "name": 'test tag 2'
-        }
-
-        tag2 = TagModel(**tag_data2)
-        tag2.save()
-        tag_id2 = tag2.id
-
-        res = self.client.get(f"/tags/{tag_id2}")
-        self.assertEqual(res.status_code, 200)
-        data = json.loads(res.data)
-        self.assertEqual(data["name"], tag_data2["name"])
+        user = TestTags.create_test_tag1_and_tag2_by_user1(self)
 
         tag_data_to_change = {
-            "name": 'test tag 2'
+            "name": 'test2 tag'
         }
 
-        res = self.client.put(f"tags/{tag1_id}", data=json.dumps(tag_data_to_change),
-                              content_type="application/json")
+        res = self.client.put("tags/1", headers=TestTags.auth_headers(self, user),
+                              data=json.dumps(tag_data_to_change), content_type="application/json")
         self.assertEqual(res.status_code, 404)
         data = json.loads(res.data)
         self.assertEqual(data["error"], f"An error occurred while changing tag"
                                         f" or a tag with such name is already exist.")
 
     def test_delete_tag_by_id(self):
-        tag_data = {
-            "name": 'test tag'
-        }
+        user = TestTags.create_test_tag1_by_user1(self)
 
-        tag = TagModel(**tag_data)
-        tag.save()
-        tag_id = tag.id
-
-        res = self.client.get(f"/tags/{tag_id}")
+        res = self.client.delete("tags/1", headers=TestTags.auth_headers(self, user))
         self.assertEqual(res.status_code, 200)
         data = json.loads(res.data)
-        self.assertEqual(data["name"], tag_data["name"])
-
-        res = self.client.delete(f"tags/{tag_id}")
-        self.assertEqual(res.status_code, 200)
-        data = json.loads(res.data)
-        self.assertEqual(data, f"Tag with id={tag_id} deleted")
+        self.assertEqual(data, "Tag with id=1 deleted")
 
     def test_delete_tag_by_id_not_found(self):
-        res = self.client.delete("tags/1")
+        user = TestTags.create_test_user1(self)
+
+        res = self.client.delete("tags/1", headers=TestTags.auth_headers(self, user))
         self.assertEqual(res.status_code, 404)
         data = json.loads(res.data)
         self.assertEqual(data["error"], "Tag with id=1 not found")
--- tests/test_users.py
+++ tests/test_users.py
@@ -15,7 +15,7 @@ class TestUsers(TestCase):
         with self.app.app_context():
             db.create_all()
 
-    def test_get_user_by_id(self):
+    def create_test_user1(self):
         user_data = {
             "username": 'admin',
             'password': 'admin'
@@ -28,6 +28,35 @@ class TestUsers(TestCase):
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(data["username"], user_data["username"])
+        return user
+
+    def create_test_user1_and_user2(self):
+        users_data = [
+            {
+                "username": 'admin1',
+                'password': 'admin1'
+            },
+            {
+                "username": 'admin2',
+                'password': 'admin2'
+            },
+        ]
+        ids_lst = []
+        for user_data in users_data:
+            user = UserModel(**user_data)
+            user.save()
+            user_id = user.id
+            ids_lst.append(user_id)
+
+        res = self.client.get('/users')
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data[0]["username"], users_data[0]["username"])
+        self.assertEqual(data[1]["username"], users_data[1]["username"])
+        return ids_lst
+
+    def test_get_user_by_id(self):
+        TestUsers.create_test_user1(self)
 
     def test_get_user_not_found_by_id(self):
         res = self.client.get('/users/1')
@@ -36,25 +65,7 @@ class TestUsers(TestCase):
         self.assertEqual(data["error"], "No user with id=1")
 
     def test_get_users(self):
-        users_data = [
-           {
-               "username": 'admin',
-               'password': 'admin'
-           },
-           {
-               "username": 'ivan',
-               'password': '12345'
-           },
-        ]
-        for user_data in users_data:
-            user = UserModel(**user_data)
-            user.save()
-
-        res = self.client.get('/users')
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data[0]["username"], users_data[0]["username"])
-        self.assertEqual(data[1]["username"], users_data[1]["username"])
+        TestUsers.create_test_user1_and_user2(self)
 
     def test_get_users_not_found(self):
         res = self.client.get("/users")
@@ -67,20 +78,14 @@ class TestUsers(TestCase):
            "username": 'admin',
            'password': 'admin'
         }
+
         res = self.client.post('/users', data=json.dumps(user_data), content_type="application/json")
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 201)
         self.assertIn("admin", data.values())
 
     def test_post_user_creation_the_same_username(self):
-        user_data = {
-            "username": 'admin',
-            'password': 'admin'
-        }
-        res = self.client.post('/users', data=json.dumps(user_data), content_type="application/json")
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 201)
-        self.assertIn("admin", data.values())
+        TestUsers.create_test_user1(self)
 
         new_user_data = {
             "username": 'admin',
@@ -94,22 +99,12 @@ class TestUsers(TestCase):
                                         "You can only add a unique name")
 
     def test_put_user_by_id(self):
-        user_data = {
-            "username": 'admin',
-            'password': 'admin'
-        }
-        user = UserModel(**user_data)
-        user.save()
-        user_id = user.id
-        res = self.client.get(f"/users/{user_id}")
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data["username"], user_data["username"])
+        user = TestUsers.create_test_user1(self)
 
         user_data_to_change = {
             "username": 'admin1'
         }
-        res = self.client.put(f"/users/{user_id}", data=json.dumps(user_data_to_change),
+        res = self.client.put(f"/users/{user.id}", data=json.dumps(user_data_to_change),
                               content_type="application/json")
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
@@ -126,51 +121,24 @@ class TestUsers(TestCase):
         self.assertEqual(data["error"], "No user with id=1")
 
     def test_put_user_by_id_change_to_the_same_username(self):
-        user_data1 = {
-            "username": 'admin1',
-            'password': 'admin1'
-        }
-        user1 = UserModel(**user_data1)
-        user1.save()
-        user_id = user1.id
-        res = self.client.get(f"/users/{user_id}")
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data["username"], user_data1["username"])
-
-        user_data1 = {
-            "username": 'admin2',
-            'password': 'admin2'
-        }
-        user2 = UserModel(**user_data1)
-        user2.save()
-        user_id = user2.id
-        res = self.client.get(f"/users/{user_id}")
-        data = json.loads(res.data)
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(data["username"], user_data1["username"])
+        users = TestUsers.create_test_user1_and_user2(self)
 
         user_data_to_change = {
             "username": 'admin1'
         }
-        res = self.client.put(f"/users/{user_id}", data=json.dumps(user_data_to_change),
+
+        res = self.client.put(f"/users/{users[1]}", data=json.dumps(user_data_to_change),
                               content_type="application/json")
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data["error"], "An error occurred while changing the user")
 
     def test_delete_by_id(self):
-        user_data = {
-            "username": 'admin',
-            'password': 'admin'
-        }
-        user = UserModel(**user_data)
-        user.save()
-        user_id = user.id
-        res = self.client.delete(f"/users/{user_id}")
+        user = TestUsers.create_test_user1(self)
+        res = self.client.delete(f"/users/{user.id}")
         data = json.loads(res.data)
         self.assertEqual(res.status_code, 200)
-        self.assertEqual(data, f"User with id={user_id} deleted")
+        self.assertEqual(data, f"User with id={user.id} deleted")
 
     def test_delete_by_id_not_found(self):
         res = self.client.delete("/users/1")
@@ -182,6 +150,5 @@ class TestUsers(TestCase):
         with self.app.app_context():
             db.session.remove()
             db.drop_all()
-
 
 # python -m unittest tests/test_users.py
