39fa18987300c18ac60fdd0e25b656e7a89b7f23
UshakovAleksandr <sashapimp@mail.ru>
2021-03-19 12:45:20 +0300

    Change Config

--- api/__init__.py
+++ api/__init__.py
@@ -6,32 +6,11 @@ from flask_migrate import Migrate
 from config import Config
 from flask_marshmallow import Marshmallow
 from flask_httpauth import HTTPBasicAuth
-from apispec import APISpec
-from apispec.ext.marshmallow import MarshmallowPlugin
 from flask_apispec.extension import FlaskApiSpec
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-security_definitions = {
-    "basicAuth": {
-        "type": "basic"
-    }
-}
-
-app.config.update({
-    'APISPEC_SPEC': APISpec(
-        title='Notes Project',
-        version='v1',
-        plugins=[MarshmallowPlugin()],
-        securityDefinitions=security_definitions,
-        security=[],
-        openapi_version='2.0.0'
-    ),
-    'APISPEC_SWAGGER_URL': '/swagger',  # URI API Doc JSON
-    'APISPEC_SWAGGER_UI_URL': '/swagger-ui'  # URI UI of API Doc
-})
 
 # logging.basicConfig(filename='record.log',
 #                    level=logging.INFO,
--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
+
+security_definitions = {
+    "basicAuth": {
+        "type": "basic"
+    }
+}
 
 
 class Config:
@@ -10,3 +18,14 @@ class Config:
     DEBUG = True
     PORT = 5000
     SECRET_KEY = 'supeR secret KeyS'
+    APISPEC_SPEC = APISpec(
+        title='Notes Project',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        securityDefinitions=security_definitions,
+        security=[],
+        openapi_version='2.0.0'
+    )
+    APISPEC_SWAGGER_URL = '/swagger'  # URI API Doc JSON
+    APISPEC_SWAGGER_UI_URL = '/swagger-ui'  # URI UI of API Doc
+
